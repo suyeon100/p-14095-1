09ebb913592efb63615f8b18a9483b97ec63ca7b
suyeon100 <xxyx9811@gmail.com>
2025-11-19 15:35:25 +0900

    h2 add

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -3,12 +3,15 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void index() {
+    @ResponseBody
+    public String index() {
         System.out.println("index() 메서드 호출됨!");
+        return "안녕하세요 SBB에 오신 것을 환영합니다.";
     }
 
     @GetMapping("/hello")
